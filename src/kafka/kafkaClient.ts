6cd5cc55909aab5605931119d26685a82e5dc645
Owais Dashti <owaisdashti@outlook.com>
2024-11-30 15:14:51 +0100

    fix: update Kafka client ID from 'order-service' to 'mtogo'

--- src/kafka/kafkaClient.ts
+++ src/kafka/kafkaClient.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const kafka = new Kafka({
-  clientId: 'order-service',
+  clientId: 'mtogo',
   brokers: [process.env.KAFKA_BROKER ?? 'kafka:9092'],
 });
 
